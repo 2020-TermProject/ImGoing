0c8d6ac647abdb54dbcc810bde88595e987ea273
Parkseongchan <38308291+Parkseongchan@users.noreply.github.com>
2020-05-05 20:21:18 +0900

    카카오톡 로그인 구현

--- app/src/main/java/com/example/a2020project/FragmentSetting.java
+++ app/src/main/java/com/example/a2020project/FragmentSetting.java
@@ -1,17 +1,52 @@
 package com.example.a2020project;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class FragmentSetting extends Fragment {
+    ImageButton loginBtn;
+    TextView nameText;
+    Context mContext;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        //return inflater.inflate(R.layout.fragment_setting, container, false);
+        View v = inflater.inflate(R.layout.fragment_setting, container, false);
+        Intent intent = ((Activity) getActivity()).getIntent();
+        String Nick_name = intent.getStringExtra("NICKNAME");
+
+        loginBtn = (ImageButton)v.findViewById(R.id.login);
+        nameText =(TextView)v.findViewById(R.id.namebox);
+        loginBtn = (ImageButton)v.findViewById(R.id.login);
+        Log.d("nickname", Nick_name + "확인되었습니다");
+        if(Nick_name != null){
+            loginBtn.setImageResource(R.drawable.logout);
+            nameText.setText(intent.getStringExtra("NICKNAME")+"님 반갑습니다. ");
+        }
+        else {
+            loginBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent intent = new Intent(getActivity(), LoginActivity.class);
+
+                    startActivityForResult(intent, sub);
+                }
+            });
+        }
+        return v;
     }
+
+    public static final int sub = 1001;
+
 }
