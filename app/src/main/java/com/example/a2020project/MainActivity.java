bc1133ca0c4f72af9f07aae35a2aaa7966d71494
han <taxol1203@pusan.ac.kr>
2020-05-06 17:41:50 +0900

    bottom navigation 시작화면 home으로 설정 #TPCX-5

--- app/src/main/java/com/example/a2020project/MainActivity.java
+++ app/src/main/java/com/example/a2020project/MainActivity.java
@@ -1,20 +1,10 @@
 package com.example.a2020project;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.net.Uri;
-import android.database.sqlite.SQLiteDatabase;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -40,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.frameLayout, fragmentSearch).commitAllowingStateLoss();
+        transaction.replace(R.id.frameLayout, fragmentHome).commitAllowingStateLoss();
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
@@ -53,12 +43,12 @@ public class MainActivity extends AppCompatActivity {
 
             switch (menuItem.getItemId())
             {
-                case R.id.search:
-                    transaction.replace(R.id.frameLayout, fragmentSearch).commitAllowingStateLoss();
-                    break;
-
                 case R.id.home:
                     transaction.replace(R.id.frameLayout, fragmentHome).commitAllowingStateLoss();
+                    break;
+
+                case R.id.search:
+                    transaction.replace(R.id.frameLayout, fragmentSearch).commitAllowingStateLoss();
                     break;
 
                 case R.id.category:
