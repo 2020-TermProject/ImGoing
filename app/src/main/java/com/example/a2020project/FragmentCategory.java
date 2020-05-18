5d5395390724a73c8a472c6c6cc29a36b8f1e284
kimmsunsuk <skrwlejvqk@pusan.ac.kr>
2020-05-18 20:16:33 +0900

    카테고리 일부 수정 및 구현        #TPCX-11

--- app/src/main/java/com/example/a2020project/FragmentCategory.java
+++ app/src/main/java/com/example/a2020project/FragmentCategory.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.content.Intent;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,4 +15,5 @@ public class FragmentCategory extends Fragment {
                              @Nullable Bundle savedInstanceState){
         return inflater.inflate(R.layout.fragment_category, container, false);
     }
-}
+
+};
---
+++ app/src/main/java/com/example/a2020project/cafefood.java
@@ -0,0 +1,4 @@
+package com.example.a2020project;
+
+public class cafefood {
+}
