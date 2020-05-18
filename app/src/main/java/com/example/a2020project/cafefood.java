1d1cb820c56024f49be1ba28a1699ebcdf91d071
jinseok7421 <jinseok7421@hanmail.net>
2020-05-18 22:42:39 +0900

    카테고리 화면이동

--- app/src/main/java/com/example/a2020project/cafefood.java
+++ app/src/main/java/com/example/a2020project/cafefood.java
@@ -1,4 +1,13 @@
 package com.example.a2020project;
+import android.os.Bundle;
 
 public class cafefood {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+public class cafefood extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cafefoodlayout);
+    }
 }
