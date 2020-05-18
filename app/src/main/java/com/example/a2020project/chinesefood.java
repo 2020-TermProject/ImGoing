f9d9c415a69e598e592307cfdde0880982dd1782
jinseok7421 <jinseok7421@hanmail.net>
2020-05-18 22:42:59 +0900

    카테고리 화면이동

--- app/src/main/java/com/example/a2020project/chinesefood.java
+++ app/src/main/java/com/example/a2020project/chinesefood.java
@@ -1,4 +1,13 @@
 package com.example.a2020project;
 
 public class chinesefood {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+public class chinesefood extends AppCompatActivity{
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chinesefoodlayout);
+    }
 }
