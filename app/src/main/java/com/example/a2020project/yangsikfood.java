7571743ea80356f17943fa3baf929f3aa6cbda8d
jinseok7421 <jinseok7421@hanmail.net>
2020-05-18 22:43:45 +0900

    카테고리 화면이동

--- app/src/main/java/com/example/a2020project/yangsikfood.java
+++ app/src/main/java/com/example/a2020project/yangsikfood.java
@@ -1,4 +1,13 @@
 package com.example.a2020project;
+import android.os.Bundle;
 
 public class yangsikfood {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+public class yangsikfood extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.yangsikfoodlayout);
+    }
 }
