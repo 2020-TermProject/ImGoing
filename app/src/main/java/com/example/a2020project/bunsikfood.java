e85193df98f2ff676393877eb3a98c9f12a39646
jinseok7421 <jinseok7421@hanmail.net>
2020-05-18 22:42:16 +0900

    카테고리 화면이동

--- app/src/main/java/com/example/a2020project/bunsikfood.java
+++ app/src/main/java/com/example/a2020project/bunsikfood.java
@@ -1,4 +1,13 @@
 package com.example.a2020project;
+import android.os.Bundle;
 
 public class bunsikfood {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+public class bunsikfood extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bunsikfoodlayout);
+    }
 }
