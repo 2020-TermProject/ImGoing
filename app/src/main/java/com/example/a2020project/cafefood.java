11e3db87fc95a99c1a7a311740489c9f1caea09a
jinseok7421 <jinseok7421@hanmail.net>
2020-05-18 22:48:51 +0900

    Update cafefood.java

--- app/src/main/java/com/example/a2020project/cafefood.java
+++ app/src/main/java/com/example/a2020project/cafefood.java
@@ -1,7 +1,6 @@
 package com.example.a2020project;
 import android.os.Bundle;
 
-public class cafefood {
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 public class cafefood extends AppCompatActivity{
