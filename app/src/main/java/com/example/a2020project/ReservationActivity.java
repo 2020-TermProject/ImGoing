a1c232b32fd43dec3605be0556d79f88c60a546c
Parkseongchan <38308291+Parkseongchan@users.noreply.github.com>
2020-06-12 20:49:29 +0900

    카카오페이 구현

--- app/src/main/java/com/example/a2020project/ReservationActivity.java
+++ app/src/main/java/com/example/a2020project/ReservationActivity.java
@@ -1,14 +1,25 @@
 package com.example.a2020project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ReservationActivity extends AppCompatActivity {
+    public Button resBtn;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation);
-
+        resBtn = (Button)findViewById(R.id.reservation_Payment);
+        resBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PaymentActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
