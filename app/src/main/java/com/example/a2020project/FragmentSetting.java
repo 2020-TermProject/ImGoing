5a2800d4ab095a7271c03b224a15f1bcd9e723e2
Parkseongchan <38308291+Parkseongchan@users.noreply.github.com>
2020-06-16 23:04:53 +0900

    가게 관리 연결

--- app/src/main/java/com/example/a2020project/FragmentSetting.java
+++ app/src/main/java/com/example/a2020project/FragmentSetting.java
@@ -12,6 +12,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.a2020project.EnrollActivity;
 
 import androidx.annotation.NonNull;
@@ -21,6 +23,7 @@ import androidx.fragment.app.Fragment;
 public class FragmentSetting extends Fragment {
     Button loginBtn;
     Button enrollBtn;
+    Button manageBtn;
     //TextView nameText;
     //Context mContext;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -33,6 +36,7 @@ public class FragmentSetting extends Fragment {
         //loginBtn = (ImageButton)v.findViewById(R.id.login);
         loginBtn = v.findViewById(R.id.login);
         enrollBtn = v.findViewById(R.id.setting_Enroll);
+        manageBtn = v.findViewById(R.id.setting_Manage);
         //nameText =(TextView)v.findViewById(R.id.namebox);
         //loginBtn = (ImageButton)v.findViewById(R.id.login);
         Log.d("nickname", Nick_name + "확인되었습니다");
@@ -41,6 +45,19 @@ public class FragmentSetting extends Fragment {
             //loginBtn.setImageResource(R.drawable.logout);
             //nameText.setText(intent.getStringExtra("NICKNAME")+"님 반갑습니다. ");
             Log.d("check", intent.getStringExtra("NICKNAME")+"  "+intent.getStringExtra("E-MAIL")+"  "+intent.getStringExtra("USER_ID"));
+            loginBtn.setText("로그아웃");
+            enrollBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent intent = new Intent(getActivity(), EnrollActivity.class);
+                    startActivityForResult(intent, sub);
+                }
+            });
+            manageBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent intent = new Intent(getActivity(), StoremanagementActivity.class);
+                    startActivityForResult(intent, sub);
+                }
+            });
         }
         else {
             loginBtn.setOnClickListener(new View.OnClickListener() {
@@ -49,15 +66,24 @@ public class FragmentSetting extends Fragment {
                     startActivityForResult(intent, sub);
                 }
             });
-
             enrollBtn.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
+                    Toast myToast = Toast.makeText(getActivity(),"로그인 후 이용해 주세요.", Toast.LENGTH_SHORT);
+                    myToast.show();
+                    //임시로 로그인 안해도 들어갈수 있게 만들어 놨습니다.
                     Intent intent = new Intent(getActivity(), EnrollActivity.class);
                     startActivityForResult(intent, sub);
                 }
             });
-
-
+            manageBtn.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Toast myToast = Toast.makeText(getActivity(),"로그인 후 이용해 주세요.", Toast.LENGTH_SHORT);
+                    myToast.show();
+                    //임시로 로그인 안해도 들어갈수 있게 만들어 놨습니다.
+                    Intent intent = new Intent(getActivity(), StoremanagementActivity.class);
+                    startActivityForResult(intent, sub);
+                }
+            });
         }
         return v;
     }
---
+++ app/src/main/java/com/example/a2020project/StoremanagementActivity.java
@@ -0,0 +1,13 @@
+package com.example.a2020project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StoremanagementActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_storemanagement);
+
+    }
+}
