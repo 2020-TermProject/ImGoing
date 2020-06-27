d45eaaeb349e6e52ea63f5893f6d945d385f00ef
Parkseongchan <38308291+Parkseongchan@users.noreply.github.com>
2020-06-27 21:34:48 +0900

    공지사항 연결 및 리뷰 리사이클러뷰

---
+++ app/src/main/java/com/example/a2020project/Recycler/ReviewRow.java
@@ -0,0 +1,11 @@
+package com.example.a2020project.Recycler;
+
+public class ReviewRow {
+    public String ID;
+    public String comment;
+
+    public ReviewRow(String ID, String comment){
+        this.ID = ID;
+        this.comment = comment;
+    }
+}
